28ff8ce64090c5c5974f2f91f725a347569bea58
Andrew Wessels <shixish@gmail.com>
2016-09-16 17:31:26 -1000

    Got the gulp watch process working, it compiles the scss, adds vendor prefixes, and uploads to ftp

--- gulpfile.js
+++ gulpfile.js
@@ -3,64 +3,77 @@ var gutil = require('gulp-util');
 var ftp = require('vinyl-ftp');
 var creds = require('./creds');
 
-var compass = require('gulp-compass');
+//Local files globs:
+var globs = [
+  'images/**',
+  'css/**',
+  'js/**',
+  'fonts/**',
+  'templates/**',
+  'favicon.ico',
+  '*.php',
+  '*.info',
+];
+
+// helper function to build an FTP connection based on our configuration
+function getFtpConnection() {  
+  return ftp.create( {
+    host: creds.host,
+    user: creds.user,
+    password: creds.password,
+    port: creds.port,
+    parallel: 8,
+    log: gutil.log
+  });
+}
  
 gulp.task('compass', function() {
+  var compass = require('gulp-compass');
+  var postcss      = require('gulp-postcss');
+  var sourcemaps   = require('gulp-sourcemaps');
+  var autoprefixer = require('autoprefixer');
+
   gulp.src('./sass/*.scss')
     .pipe(compass({
       config_file: './config.rb',
       css: 'css',
       sass: 'sass'
     }))
-    .pipe(gulp.dest('app/assets/temp'));
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer({ browsers: ['last 4 versions'] }) ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('css'));
 });
 
-gulp.task('deploy', function () {
-	console.log(creds);
+gulp.task('deploy', ['compass'], function () {
+  var conn = getFtpConnection();
 
-    var conn = ftp.create( {
-        host: creds.host,
-        user: creds.user,
-        password: creds.password,
-        port: creds.port,
-        parallel: 10,
-        log: gutil.log
-    });
+  // using base = '.' will transfer everything to /public_html correctly
+  // turn off buffering in gulp.src for best performance
 
-    var globs = [
-        'images/**',
-        'css/**',
-        'js/**',
-        'fonts/**',
-        'templates/**',
-        'favicon.ico',
-        '*.php',
-        '*.info',
-    ];
+  return gulp.src(globs, {base: '.', buffer: false})
+    .pipe(conn.newer(creds.remote)) // only upload newer files
+    .pipe(conn.dest(creds.remote));//'/public_html'
+});
 
-    // using base = '.' will transfer everything to /public_html correctly
-    // turn off buffering in gulp.src for best performance
+gulp.task('watch', function() {
+  gulp.watch(['./sass/*.scss', './sass/**/*.scss'], ['compass']);
 
-    return gulp.src(globs, {base: '.', buffer: false})
-        .pipe(conn.newer(creds.remote)) // only upload newer files
-        .pipe(conn.dest(creds.remote));//'/public_html'
+  // gulp.watch(globs, ['deploy']); //If we do this, it would upload every thing any time you save something...
+  var conn = getFtpConnection();
+  gulp.watch(globs)
+  .on('change', function(event) {
+    console.log('Changes detected! Uploading file "' + event.path + '", ' + event.type);
+
+    //Only upload the changed files:
+    return gulp.src( [event.path], { base: '.', buffer: false } )
+      .pipe(conn.newer(creds.remote)) // only upload newer files 
+      .pipe(conn.dest(creds.remote))
+    ;
+  });
 });
 
 // gulp.task('default', function () {
-// 	console.log('Hello Gulp!');
+//   gulp.src('./sass/*.scss').pipe(pipeTest());
 // });
 
-// gulp.task('ftp-deploy-watch', function() {
-
-//     var conn = getFtpConnection();
-
-//     gulp.watch(localFilesGlob)
-//     .on('change', function(event) {
-//       console.log('Changes detected! Uploading file "' + event.path + '", ' + event.type);
-
-//       return gulp.src( [event.path], { base: '.', buffer: false } )
-//         .pipe( conn.newer( remoteFolder ) ) // only upload newer files 
-//         .pipe( conn.dest( remoteFolder ) )
-//       ;
-//     });
-// });
